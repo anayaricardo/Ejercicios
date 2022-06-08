9667e33c10022c0dc1a2f0e171fb857ef8dd352d
anayaricardo <einstein.letsgetrelative@gmail.com>
2022-06-08 13:52:49 -0500

    Actualización

---
+++ Dom/index.js
@@ -0,0 +1,7 @@
+//console.log(document.getElementsByTagName('h1'));
+
+const h1Collection = document.getElementsByTagName("h1");
+
+h1Collection.forEach((element) => {
+  console.log(element);
+});
---
+++ w3schools.com/index.js
@@ -0,0 +1,27 @@
+// Exercise 1. Create a variable called carName, assign the value Volvo to it.
+
+var carName = "Volvo";
+
+// Exercise 2. Create a variable called x, assign the value 50 to it.
+
+var x = 50;
+
+// Exercise 3. Display the sum of 5 + 10, using two variables: x and y.
+
+var x = 5;
+var y = 10;
+document.getElementById("demo").innerHTML = x + y;
+
+// Exercise 4. Create a variable called z, assign x + y to it, and display in an alert box.
+
+var x = 5;
+var y = 10;
+var z = x + y;
+alert(z);
+
+// Exercise 5. On one single line, declare three variables with the following names and values:
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+var firstName = "John", lastName = "Doe", age = 35;
